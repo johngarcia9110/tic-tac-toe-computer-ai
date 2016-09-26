003bc0eb215bf45092d9b9e5669cba72034e71ef
John Garcia <johngarcia9110@gmail.com>
2016-09-25 22:42:05 -0400

    added comments to js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 'use strict';
 
-
+//testing alert
 var getBox = function(arg) {
   return function() {
     alert(arg.id);
   };
 };
 
+//get boxes (td)
 var boxes = document.getElementsByClassName("box");
 
+
 for (var i = 0, l = boxes.length; l > i; i++){
+    //when box is click use getBox function to output td id
     boxes[i].onclick = getBox(boxes[i]);
 }
   
